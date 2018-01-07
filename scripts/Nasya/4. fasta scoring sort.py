a7ccbe268c9e90b3223d5e308311190a8a71dc10
KIRILLxBREAKm <kikochnev@gmail.com>
2018-01-08 01:54:21 +0300

    read only 1000 row

--- scripts/Nasya/4. fasta scoring sort.py
+++ scripts/Nasya/4. fasta scoring sort.py
@@ -2,7 +2,7 @@ from Bio import SeqIO
 
 D = []
 S = {}
-for seq_record in SeqIO.parse("fasta_example.fasta", "fasta"):
+for seq_record in SeqIO.parse("fasta_example1.fasta", "fasta"):
     print(seq_record.id, len(seq_record))
     print(repr(seq_record.seq))
     D.append(str(seq_record.seq))
@@ -10,12 +10,15 @@ for seq_record in SeqIO.parse("fasta_example.fasta", "fasta"):
 
 print(len(S))
 
-mat = {('A', 'A'): 1, ('A', 'T'): 0.5, ('A', 'G'): 0.1, ('A', 'C'): 0.1,
-    ('T', 'A'): 0.5, ('T', 'T'): 1, ('T', 'G'): 0.1, ('T', 'C'): 0.1,
-    ('G', 'A'): 0.1, ('G', 'T'): 0.1, ('G', 'G'): 1, ('G', 'C'): 0.5,
-    ('C', 'A'): 0.1, ('C', 'T'): 0.1, ('C', 'G'): 0.5, ('C', 'C'): 1}
+mat = {('A','A'): 1, ('A','T'): 0.5, ('A','G'): 0.1, ('A','C'): 0.1,
+    ('T','A'): 0.5, ('T','T'): 1, ('T','G'): 0.1, ('T','C'): 0.1,
+    ('G','A'): 0.1, ('G','T'): 0.1, ('G','G'): 1, ('G','C'): 0.5,
+    ('C','A'): 0.1, ('C','T'): 0.1, ('C','G'): 0.5, ('C','C'): 1}
 
 indel= 0.5 
+
+print(mat[('A','A')])
+print(mat[('T','C')], "yes")
 
 def scoringLevDistance(v, w, is_print=False):
     n = len(v)
@@ -38,6 +41,7 @@ def scoringLevDistance(v, w, is_print=False):
             a = v[i-1]
             b = w[j-1]
             
+            print(mat[('T','C')], i, j)
             D[i][j] = D[i-1][j-1]+mat[(a,b)]
             if a==b:
                 opt[i][j] = 0 # NO SUBSTITUTE   
@@ -86,14 +90,37 @@ def scoringLevDistance(v, w, is_print=False):
 
 seqNominal = "TAAACAATTTACCCAACCTC" # seqNominal = input()
 print("seqNominal: ", seqNominal)
-#print(scoringLevDistance(seqNominal, S['seq0']))
+
+def heapsort(lst):
+    ''' Heapsort. Note: this function sorts in-place (it mutates the list). '''
+    # in pseudo-code, heapify only called once, so inline it here
+    for start in range(int(len(lst)/2), -1, -1):#range(int((len(lst)-2)/2), -1, -1):
+        siftdown(lst, start, len(lst)-1)
+ 
+    for end in range(len(lst)-1, 0, -1):
+        lst[end], lst[0] = lst[0], lst[end]
+        siftdown(lst, 0, end-1)
+    return lst
+ 
+def siftdown(lst, start, end):
+    root = start
+    while True:
+        child = root * 2 + 1
+        if child > end: break
+        if child + 1 <= end and lst[child] < lst[child + 1]:
+            child += 1
+        if lst[root] < lst[child]:
+            lst[root], lst[child] = lst[child], lst[root]
+            root = child
+        else:
+            break
 
 dictScore =  {key: scoringLevDistance(seqNominal, S[key]) for key in S} # dictionary comprehension
-print(dictScore)
-sortDictScore = {key: dictScore[key] for key in sorted(dictScore, key=dictScore.get, reverse=False)}
-print(sortDictScore)
+#print(dictScore)
+sortDictScore = {key: dictScore[key] for key in heapsort(key=dictScore.get)}
+#print(sortDictScore)
 
 for k in sortDictScore:
     print(k)
     scoringLevDistance(seqNominal, S[k], True)
-    print()
+    print()"""
