ddb6b9e62d3e31d0e311106d59d57dee4eb2afb6
KIRILLxBREAKm <kikochnev@gmail.com>
2018-02-22 23:16:00 +0300

    full genome tss seq and fisher test (overlaping with promoters)

--- data/MARA/CAGE-hg19/read cage data.R
+++ data/MARA/CAGE-hg19/read cage data.R
@@ -1,25 +1,31 @@
 # read file
-setwd('/home/kirill/Documents/Projects/bioinformatics/data/MARA/')
+setwd('/home/kirill/Documents/Projects/bioinformatics/data/MARA/CAGE-hg19/')
+
+# http://fantom.gsc.riken.jp/5/datafiles/phase2.0/extra/CAGE_peaks/hg19.cage_peak_phase1and2combined_tpm_ann.osc.txt.gz
+# tail -n +1841 robust_phase1_pls_2.tpm.desc121113.osc.txt.gz.tmp | head -n 1
+# 1838 - column namesss
+# 1839 - 01STAT:MAPPED	
+# 1840 - 02STAT:NORM_FACTOR
+# 1841 - chr10:100013403..100013414,-
 if( !any(grepl("readr", installed.packages())) ) {
   install.packages("readr")
 }
 library(readr)
-# http://fantom.gsc.riken.jp/5/datafiles/phase2.0/extra/CAGE_peaks/hg19.cage_peak_phase1and2combined_tpm_ann.osc.txt.gz
-# tail -n +1841 robust_phase1_pls_2.tpm.desc121113.osc.txt.gz.tmp | head -n 1
-df <- read_table2("robust_phase1_pls_2.tpm.desc121113.osc.txt.gz.tmp", skip=1839, n_max = 100)
+df <- read_table2("robust_phase1_pls_2.tpm.desc121113.osc.txt.gz.tmp", col_names = FALSE, skip=1840)#, n_max = 100)
 #problems(df)
-
 # parse TSS (transcription starting sites) ranges
 chr <- df[[1]]
 #save(chr, file="chr.rd"); load("chr.rd") 
-print(chr)
-chr = sapply(chr, function(y) gsub(",", ":", y))
+chr <- gsub(",", ":", chr) # меняем запятую на двоеточие
+print(head(chr))
+
 if( !any(grepl("GenomicRanges", installed.packages())) ) {
   install.packages("GenomicRanges")
 }
 library('GenomicRanges')
-chr = as(chr, "GRanges") # http://web.mit.edu/~r/current/arch/i386_linux26/lib/R/library/GenomicRanges/html/GRanges-class.html
+chr <- as(chr, "GRanges") # http://web.mit.edu/~r/current/arch/i386_linux26/lib/R/library/GenomicRanges/html/GRanges-class.html
 print(head(chr))
+sum(width(chr))/length(chr) # средняя длина
 
 # http://www.bioconductor.org/packages/release/data/annotation/manuals/BSgenome.Hsapiens.UCSC.hg19/man/BSgenome.Hsapiens.UCSC.hg19.pdf
 if( !any(grepl("TxDb.Hsapiens.UCSC.hg19.knownGene", installed.packages())) ) {
@@ -28,8 +34,27 @@ if( !any(grepl("TxDb.Hsapiens.UCSC.hg19.knownGene", installed.packages())) ) {
 }
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
-promoters(txdb)
+promoters <- promoters(txdb)
+sum(width(promoters))/length(promoters)
 
+#ov <- findOverlaps(chr, promoters) # any type of overlap (start, end, within, equal)
+# length(unique(queryHits(ov))) / length(promoters)
+# length(unique(subjectHits(ov))) / length(promoters)
+prom <- reduce(promoters, ignore.strand = FALSE)
+peaks <- reduce(chr)
+both <- intersect(prom, peaks)
+only.prom <- setdiff(prom, both)
+only.peaks <- setdiff(peaks, both)
+overlapMat <- matrix(0,, ncol = 2, nrow = 2)
+colnames(overlapMat) <- c("in.peaks", "out.peaks")
+rownames(overlapMat) <- c("in.promoters", "out.promoter")
+overlapMat[1,1] <- sum(width(both))
+overlapMat[1,2] <- sum(width(only.prom))
+overlapMat[2,1] <- sum(width(only.peaks))
+overlapMat[2,2] <- 1.5*10^9 - sum(overlapMat)
+#round(overlapMat / 10^6, 2)
+oddRatio <- overlapMat[1,1] * overlapMat[2,2] / (overlapMat[2,1] * overlapMat[1,2])
+oddRatio
 
 
 # Seqs
