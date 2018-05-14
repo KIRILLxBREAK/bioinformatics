3934e8eeb839b70c1d76b11c33275324b3bbe4c2
KIRILLxBREAKm <kikochnev@gmail.com>
2018-05-14 11:10:26 +0300

    merge fix

--- data/MARA/sarus/threshold_cut.py
+++ data/MARA/sarus/threshold_cut.py
@@ -18,7 +18,7 @@ df.drop(['thresholds'], axis=1, inplace=True)
 
 
 # сделать фильтрацию (оставить только f1 и si)
-"""df['suffix'] = df['promoters'].apply(lambda x: x.split('_')[1])
+df['suffix'] = df['promoters'].apply(lambda x: x.split('_')[1])
 df['prom'] = df['promoters'].apply(lambda x: x.split('_')[0])
 df2 = df.iloc[:, -2:].copy()
 suffix_score = {"si" : 1, "f1" : 2, "f2" : 3, "do" : 4}
@@ -31,12 +31,12 @@ df3 = df3.to_frame().reset_index()
 suffix_score_obr = {1 : "_si", 2 : "_f1", 3 : "_f2", 4 : "_do"}
 df3["suffix"] = df3['suffix_score'].map(suffix_score_obr)
 df3['promoters'] = df3['prom'] + df3['suffix']
-promoters_list = df3['promoters'].tolist()"""
+promoters_list = df3['promoters'].tolist()
 
 # список промотеров в файле
-with open("promoters_list.txt", 'w') as f:
+"""with open("promoters_list.txt", 'w') as f:
     for s in promoters_list:
-        f.write(str(s) + '\n')
+        f.write(str(s) + '\n')"""
 
 df = df[df.promoters.isin(promoters_list)]
 #df = df.set_index(['promoters'])
