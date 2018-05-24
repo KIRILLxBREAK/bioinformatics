28c33ab4e66a96c42b166a3e8bf98bd1b3a54f26
KIRILLxBREAKm <kikochnev@gmail.com>
2018-05-24 17:50:35 +0300

    add shebang

--- data/MARA/sarus/motif_occurences.sh
+++ data/MARA/sarus/motif_occurences.sh
@@ -20,4 +20,4 @@ motifPath="$(basename $i .pwm)"
 threshold="$(python3 motif_treshold_finding.py ${motifPath})"
 (echo "${motifPath}" && echo "${threshold}" ; java -cp sarus.jar ru.autosome.SARUS "../../seqs/hg19_promoters.mfa" $i besthit \
  | grep -v ">" | cut -f 1 | sed 's/-Infinity/0/') | paste -s -d ',' >> result.csv
-done
+done                                                                                
