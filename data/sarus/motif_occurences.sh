d1dd779d4d724a3911bccc896aa25a5911d37e74
KIRILLxBREAK <kikochnev@gmail.com>
2018-06-12 20:05:48 +0300

    refactor sarus

--- data/sarus/motif_occurences.sh
+++ data/sarus/motif_occurences.sh
@@ -9,15 +9,15 @@ set -o pipefail
 
 
 # переменные
-motifs=$"../HOCOMOCO/PWM"
-motifs1=$"../HOCOMOCO/PWM1"
+motifs=$"../MARA/HOCOMOCO/PWM"
+motifs1=$"../MARA/HOCOMOCO/PWM1"
 
 
-(echo "promoters" && echo "thresholds"; cat "../../seqs/hg19_promoters.mfa" | grep ">") | paste -s -d ',' > result.csv
+(echo "promoters" && echo "thresholds"; cat "../seqs/hg19_promoters.mfa" | grep ">") | paste -s -d ',' > result.csv
 for i in ${motifs1}/*.pwm #shell expansion (filename expansion)
 do
 motifPath="$(basename $i .pwm)"
 threshold="$(python3 motif_treshold_finding.py ${motifPath})"
-(echo "${motifPath}" && echo "${threshold}" ; java -cp sarus.jar ru.autosome.SARUS "../../seqs/hg19_promoters.mfa" $i besthit \
+(echo "${motifPath}" && echo "${threshold}" ; java -cp sarus.jar ru.autosome.SARUS "../seqs/hg19_promoters.mfa" $i besthit \
  | grep -v ">" | cut -f 1 | sed 's/-Infinity/0/') | paste -s -d ',' >> result.csv
-done                                                                                
+done
---
+++ data/sarus/threshold_cut.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import os
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('result.csv', index_col='promoters')
+#df.set_index(['promoters'], inplace=True)
+
+df = df.mask(df.lt(df['thresholds'], axis=0), 0)    # обнулить все значения ниже порога
+#df.iloc[:,1:] = df.iloc[:,1:].apply(lambda x: np.where(x > df.thresholds, x, 0), axis=0)
+
+cols = df.columns.values.tolist()
+cols.insert(0, "motifs")
+df = df.reset_index()
+df.drop(['thresholds'], axis=1, inplace=True)
+
+
+
+# сделать фильтрацию (оставить только f1 и si)
+df['suffix'] = df['promoters'].apply(lambda x: x.split('_')[1])
+df['prom'] = df['promoters'].apply(lambda x: x.split('_')[0])
+df2 = df.iloc[:, -2:].copy()
+suffix_score = {"si" : 1, "f1" : 2, "f2" : 3, "do" : 4}
+suffix_score_obr = {"1" : "si", "2" : "f1", "3" : "f2", "4" : "do"}
+df2['suffix_score'] = df2['suffix'].map(suffix_score)
+#print(df2.head(10))
+grouped = df2['suffix_score'].groupby(df['prom'])
+df3 = grouped.min()
+df3 = df3.to_frame().reset_index()
+suffix_score_obr = {1 : "_si", 2 : "_f1", 3 : "_f2", 4 : "_do"}
+df3["suffix"] = df3['suffix_score'].map(suffix_score_obr)
+df3['promoters'] = df3['prom'] + df3['suffix']
+promoters_list = df3['promoters'].tolist()
+
+# список промотеров в файле
+"""with open("promoters_list.txt", 'w') as f:
+    for s in promoters_list:
+        f.write(str(s) + '\n')"""
+
+df = df[df.promoters.isin(promoters_list)]
+#df = df.set_index(['promoters'])
+df.drop(['suffix','prom'], axis=1, inplace=True)
+
+
+M = df.values
+M = M.T
+#print(M)
+cols.remove('thresholds')#; cols.remove('motifs')
+#print(len(cols))
+df = pd.DataFrame(M, index=cols)
+#print(df.head(10))
+
+df.to_csv('../../analysis/M.csv', sep=',', header=False)
