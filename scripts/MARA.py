10a266572520c9cd53c3328530e3fd0657737396
KIRILLxBREAK <kikochnev@gmail.com>
2018-06-23 11:49:37 +0300

    add motif activity

--- scripts/MARA.py
+++ scripts/MARA.py
@@ -12,13 +12,18 @@ np.save('M.npy', M)
 np.save('E.npy', E)
 np.save('A.npy', A)
 
+print(M.shape)
+print(E.shape)
+print(A.shape)
+
 U, D, Vt = np.linalg.svd(M, full_matrices=False)
 D = np.diag(D)
 
 from scipy import linalg
 temp = np.dot( linalg.inv(D.T.dot(D)), D.T ).dot(U.T)
 EA = np.dot(np.dot(Vt.T, temp), E)
+np.save('EA.npy', EA)
+np.savetxt('EA.csv', EA, delimiter=',')
 
-ACT = EA/A
-
-np.savetxt('act.csv', ACT, delimiter=',')
+#ACT = EA/A
+#np.savetxt('act.csv', ACT, delimiter=',')
